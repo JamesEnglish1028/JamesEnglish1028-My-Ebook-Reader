f8a36d4da807f9a10dac3a68f7a36daee806984d
James English <jce1028@gmail.com>
2025-10-08 10:46:51 -0400

    test: add Vitest setup with jest-dom and jsdom env

---
+++ vitest.setup.ts
@@ -0,0 +1,4 @@
+import '@testing-library/jest-dom';
+
+// jsdom is the default for Vitest in many setups but ensure globals are present.
+// If any global polyfills are needed (DOMParser, etc) they can be added here.
